549563e3c6afe4533e73cd90270f3cd0fe044867
Vinicius Alvim <vini0191@gmail.com>
2021-02-02 19:54:19 -0300

    feat: ensure ExpressRouteAdapter returns the error message instead of the error name

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -9,5 +9,11 @@ export const adaptRoute = (controller: Controller) => async (
     body: req.body,
   };
   const httpResponse = await controller.handle(httpRequest);
-  res.status(httpResponse.statusCode).json(httpResponse.body);
+  if (httpResponse.statusCode === 200) {
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+  } else {
+    res.status(httpResponse.statusCode).json({
+      error: httpResponse.body.message,
+    });
+  }
 };
