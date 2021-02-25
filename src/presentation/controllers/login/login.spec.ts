6372c6c9479fcbb9d4863316487b0ffe6ca5a603
Vinicius Alvim <vini0191@gmail.com>
2021-02-25 17:26:21 -0300

    test: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,6 +3,7 @@ import {
   badRequest,
   serverError,
   unauthorized,
+  ok,
 } from '../../helpers/http-helper';
 import { MissingParamError, InvalidParamError } from '../../errors';
 import { EmailValidator, HttpRequest, Authentication } from './login-protocols';
@@ -118,5 +119,11 @@ describe('Login Controller', () => {
       .mockReturnValueOnce(new Promise((_, reject) => reject(new Error())));
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test('should return 200 if valid credentials are provided', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }));
   });
 });
